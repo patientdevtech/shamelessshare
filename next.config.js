5618eda02041b23030169d39727eed1c0e5e14df
katiemcmillin <katiemcmillin@gmail.com>
2022-09-16 15:03:58 -0400

    OAuth working

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+};
